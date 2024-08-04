6becc74716568bcfdadcb590f180d6ffe251fea1
dheeraj404 <130442066+dheeraj404@users.noreply.github.com>
2024-08-05 00:28:39 +0530

    fix font text chang

--- src/Component/ClientSlider.js
+++ src/Component/ClientSlider.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import './ClientSlider.css';
 
 const clients = [
@@ -8,32 +9,65 @@ const clients = [
   { id: 3, logo: 'Client.png' },
   { id: 4, logo: 'Client.png' },
   { id: 5, logo: 'Client.png' },
-  { id: 6, logo: 'Client.png' }, // Add more clients as needed
+  { id: 6, logo: 'Client.png' },
   { id: 7, logo: 'Client.png' },
   { id: 8, logo: 'Client.png' },
   { id: 9, logo: 'Client.png' },
   { id: 10, logo: 'Client.png' }
 ];
 
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 7,
+    slidesToSlide: 1
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5,
+    slidesToSlide: 1
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+    slidesToSlide: 1
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2,
+    slidesToSlide: 1
+  }
+};
+
 const ClientSlider = () => {
   return (
     <div className="client-slider-container">
       <h2 className="text-center">Our Clients</h2>
-      <Carousel indicators={false} controls={true} interval={null}>
-        {clients.slice(0, clients.length / 5).map((_, index) => (
-          <Carousel.Item key={index}>
-            <div className="d-flex justify-content-center">
-              {clients.slice(index * 5, index * 5 + 5).map((client) => (
-                <div className="client-logo-container" key={client.id}>
-                  <img
-                    src={client.logo}
-                    alt={`Client ${client.id}`}
-                    className="client-logo"
-                  />
-                </div>
-              ))}
-            </div>
-          </Carousel.Item>
+      <Carousel
+        swipeable={true}
+        draggable={true}
+        showDots={true}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        // autoPlay={true}
+        // autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={['tablet', 'mobile']}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
+        {clients.map((client) => (
+          <div className="client-logo-container" key={client.id}>
+            <img
+              src={client.logo}
+              alt={`Client ${client.id}`}
+              className="client-logo"
+            />
+          </div>
         ))}
       </Carousel>
     </div>
